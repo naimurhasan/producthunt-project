d556a189c5b04c23989a66f56ee93a78ad1e66c1
Naimur Hasan <imuhasan98@gmail.com>
2020-10-08 14:47:14 +0600

    LOGIN LOGOUT DONE

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'Username or password didn\'t match'})
+
     return render(request, 'accounts/login.html')
 
 def signup(request):
-    pass
+    if request.method == 'POST':
+        # verify confirm pass
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # check if username taken
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Username already taken'})
+            except User.DoesNotExist:
+                # create uuser
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Password Doesn\'t math.'})
+
+    return render(request, 'accounts/signup.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('home')
